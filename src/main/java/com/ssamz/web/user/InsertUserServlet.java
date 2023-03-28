06adaf37bab848419138fc3ed8bf10b55d9770df
kdm1633 <kdm1633@outlook.com>
2023-03-28 12:04:41 +0900

    Ch05 - User request processing

---
+++ src/main/java/com/ssamz/web/user/InsertUserServlet.java
@@ -0,0 +1,51 @@
+package com.ssamz.web.user;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ssamz.biz.user.UserDao;
+import com.ssamz.biz.user.UserVo;
+
+public class InsertUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		String name = request.getParameter("name");
+		String role = request.getParameter("role");
+		String selfInfo = request.getParameter("selfInfo");
+		String[] languages = request.getParameterValues("languages");
+		String age = request.getParameter("age");
+		
+		System.out.println("아이디 : " + id);
+		System.out.println("비밀번호 : " + password);
+		System.out.println("이름 : " + name);
+		System.out.println("권한 : " + role);
+		System.out.println("자기소개 : " + selfInfo);
+		System.out.println("언어 경험");
+		for (String language : languages)
+			System.out.print(language + ", ");
+		System.out.println();
+		System.out.println("나이 : " + age);
+		
+		// DB connection
+		
+		UserVo vo = new UserVo();
+		vo.setId(id);
+		vo.setPassword(password);
+		vo.setName(name);
+		vo.setRole(role);
+		
+		UserDao dao = new UserDao();
+		dao.insertUser(vo);
+		
+		response.sendRedirect("login.html");
+	}
+}
